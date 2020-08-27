242950d8555c3be75b9ad754e21273685da80909
ketulkumar <k2lsuthar@gmail.com>
2020-08-27 14:31:56 -0400

    Added product class

---
+++ SaleTaxDecorator/src/main/java/SalesTax/Product.java
@@ -0,0 +1,57 @@
+package SalesTax;
+
+public class Product implements ProductItem {
+	
+	private String productName;
+	private double  productPrice;
+	private boolean isProductImported;
+	private boolean isProductExempt;
+	
+	public Product(String name, double price) {
+		this.productName = name;
+		this.productPrice = price;
+		this.isProductImported = false;
+		this.isProductExempt = false;
+	}
+
+	public boolean isProductExempt() {
+		return isProductExempt;
+	}
+
+	public void setProductExempt(boolean isProductExempt) {
+		this.isProductExempt = isProductExempt;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public void setProductPrice(double productPrice) {
+		this.productPrice = productPrice;
+	}
+
+	public void setProductImported(boolean isProductImported) {
+		this.isProductImported = isProductImported;
+	}
+
+	public String getProductName() {
+		return this.productName;
+	}
+
+	public boolean isProductImported() {
+		return this.isProductImported;
+	}
+
+	public boolean isProductExcepmt() {
+		return this.isProductExempt;
+	}
+
+	public double getProductPrice() {
+		return this.productPrice;
+	}
+
+	public double getProductPriceWithTax() {
+		return this.productPrice;
+	}
+
+}

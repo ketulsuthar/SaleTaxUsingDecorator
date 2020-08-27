4da40e23e2a3f2a914961d849f7367da85857211
ketulkumar <k2lsuthar@gmail.com>
2020-08-27 14:57:54 -0400

    created decorator for imported product tax

---
+++ SaleTaxDecorator/src/main/java/saleTax/ImportedTaxDecorator.java
@@ -0,0 +1,37 @@
+package saleTax;
+
+public class ImportedTaxDecorator extends MainTaxDecorator {
+
+private ProductItem prod;
+	
+	final double rate = 0.05;
+
+	
+	public ImportedTaxDecorator(ProductItem prod) {
+		
+		super(prod);
+		this.prod = prod;
+		
+	}
+	public String getProductName() {
+		return this.prod.getProductName();
+	}
+
+	public boolean isProductImported() {
+		return this.prod.isProductImported();
+	}
+
+	public boolean isProductExcepmt() {
+		return this.prod.isProductExcepmt();
+	}
+
+	public double getProductPrice() {
+		return this.prod.getProductPrice();
+	}
+
+	@Override
+	double getTaxRate() {
+		return rate;
+	}
+
+}

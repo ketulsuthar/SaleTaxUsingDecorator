c12877eb76f41d93bad5092625b7cb0b6e64bc14
ketulkumar <k2lsuthar@gmail.com>
2020-08-27 15:14:17 -0400

    added productParser class

---
+++ SaleTaxDecorator/src/main/java/saleTax/ProductsParser.java
@@ -0,0 +1,42 @@
+package saleTax;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class ProductsParser {
+	
+	private static final String PRODUCT_REGEX = "(\\d+)\\s((\\w+\\s)+)at\\s(\\d+.\\d+)";
+	
+	public static ProductItem parseProduct(String line) {
+		
+		Matcher matcher = matching(line);
+		String productName = matcher.group(2).trim();
+	
+		Product item = new Product(productName, Double.valueOf(matcher.group(4)));
+		if(productName.contains("imported"))
+			item.setProductImported(true);
+		if(Utility.isExempt(productName))
+			item.setProductExempt(true);
+	
+		return item;
+	}
+	
+	public static int count(String line) {
+		return Integer.valueOf(matching(line).group(1));
+	}
+	
+	public static Matcher matching(String line) {
+		Pattern pattern = Pattern.compile(PRODUCT_REGEX);
+		Matcher matcher = pattern.matcher(line);
+		matcher.find();
+		return matcher;
+	}
+	
+	public static boolean productMatched(String productsStr) {
+		return Pattern.matches(PRODUCT_REGEX, productsStr);
+	}
+	
+
+
+}
